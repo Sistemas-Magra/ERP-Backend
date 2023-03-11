32180d748feafad05283c07fa2ed30466ff4218e
Sistemas Magra <sistemas@magrasac.com>
2023-03-11 07:56:47 -0500

    first commit

---
+++ src/main/java/com/example/magra/erp/MagraErpApplication.java
@@ -0,0 +1,13 @@
+package com.example.magra.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagraErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagraErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/magra/erp/MagraErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magra.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagraErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
